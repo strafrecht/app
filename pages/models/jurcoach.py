52f22441192f1c18a277aedd70ff2f443fd7bc3e
Jakob Bach <65466068+jacquesbach@users.noreply.github.com>
2021-10-02 11:59:14 +0200

    Update jurcoach.py

--- pages/models/jurcoach.py
+++ pages/models/jurcoach.py
@@ -5,21 +5,8 @@ from wagtail.core.fields import RichTextField
 from wagtail.images.models import Image
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.admin.edit_handlers import FieldPanel
-from wagtailmodelchooser.blocks import ModelChooserBlock
-from wagtailmodelchooser.edit_handlers import ModelChooserPanel
-from wagtailpolls.models import Poll
+from wagtailpolls.widgets import AdminPollChooser
 
-
-class PollChooser():
-    poll = ModelChooserPanel('wagtailpolls.Poll')
-
-    class Meta:
-        template = 'blocks/sidebar/poll.html'
-
-    def get_context(self, value, parent_context=None):
-        ctx = super().get_context(value, parent_context=parent_context)
-        ctx['page'] = {'poll': Poll.objects.get(id=1)}
-        return ctx
 
 class JurcoachPage(Page):
     body = RichTextField(blank=True)
@@ -33,6 +20,6 @@ class JurcoachPage(Page):
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('header'),
-        PollChooser()
+        AdminPollChooser()
     ]
     
