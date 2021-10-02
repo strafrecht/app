bb081f52278827e3f5424232b5f09ad20d0dc046
Jakob Bach <65466068+jacquesbach@users.noreply.github.com>
2021-10-02 18:41:03 +0200

    Update jurcoach.py

--- pages/models/jurcoach.py
+++ pages/models/jurcoach.py
@@ -43,7 +43,7 @@ class JurcoachPage(Page):
     content_panels = Page.content_panels + [
         ImageChooserPanel('header'),
         MultiFieldPanel(
-            [InlinePanel('jurcoachcarousel', max_num=10, min_num=0, label='Slide')],
+            [InlinePanel('jurcoachcarousel', max_num=10, min_num=0, label='Slide1')],
             heading='Slider',
         ),
     ]
