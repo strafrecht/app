331a231d9321ae12b860b572cba51580fc816108
Jakob Bach <65466068+jacquesbach@users.noreply.github.com>
2021-10-02 12:34:08 +0200

    Update jurcoach.py

--- pages/models/jurcoach.py
+++ pages/models/jurcoach.py
@@ -8,7 +8,7 @@ from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from modelcluster.fields import ParentalKey
 
 class JurcoachCarousel(Orderable):
-    page = ParentalKey("pages.models.JurcoachPage", related_name="jurcoach_carousel")
+    page = ParentalKey('JurcoachPage', related_name='jurcoachcarousel')
     illustration_choices = [
         ('falltraining', 'Falltraining'),
         ('wiki', 'Problemfeldwiki'),
@@ -43,7 +43,7 @@ class JurcoachPage(Page):
     content_panels = Page.content_panels + [
         ImageChooserPanel('header'),
         MultiFieldPanel(
-            [InlinePanel('jurcoach_carousel', max_num=10, min_num=1, label='Slide')],
+            [InlinePanel('jurcoachcarousel', max_num=10, min_num=1, label='Slide')],
             heading='Slider',
         ),
     ]
