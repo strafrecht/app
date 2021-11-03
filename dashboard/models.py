3fd0be261dd2050ee0044024991d3eda202f5a85
sergio <sfvsound@gmail.com>
2021-11-03 17:28:55 +0100

    reward models

---
+++ dashboard/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from core.models import Submission
+
+
+"""
+Motivation: to encourage Strafrecht users to contribute
+
+Reward Actions:
+    Complete MCT session - less reward points (later)
+    Wiki (Create/Update) - more reward points
+    MCT (Create/Update) - more reward points
+    Falltraining (Create/Update) - more reward points
+
+e.g.:
+
+Finishing MCT - 5 point
+Trivial contribution - 5/10 points
+Significant contribution - 50/100 points
+"""
+
+
+class Reward(models.Model):
+    name = models.CharField(max_length=100)
+    points = models.IntegerField(default=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class RewardType(models.Model):
+    submission_id = models.ForeignKey(Submission)
+    reward_type_id = models.ForeignKey(Reward)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
