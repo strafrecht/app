b93d20891024c433d7d80e84ce75296bf7bc2afe
Marek Kralewski <mck@tuxwerk.de>
2022-11-02 11:04:37 +0100

    fix email migration

--- emails/migrations/0001_initial.py
+++ emails/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('birdsong', '0005_alter_receipt_success'),
+        ('birdsong', '0004_campaign_status'),
     ]
 
     operations = [
