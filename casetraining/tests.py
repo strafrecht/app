d1b5d99343cfbd6a012932a0892976bf44aab8ee
Marek Kralewski <mck@tuxwerk.de>
2023-02-09 15:45:05 +0100

    fix tests

--- casetraining/tests.py
+++ casetraining/tests.py
@@ -14,6 +14,7 @@ class CasetrainingTestCase(TestCase):
             name="Test case 1",
             difficulty="advanced",
             user=user,
+            steps="",
             facts="Case facts"
         )
 
@@ -28,16 +29,10 @@ class ViewTestCase(TestCase):
     def setUp(self):
         user = User.objects.create(username='testuser', password='12345')
         poll = Poll.objects.create(title="Poll Title")
-        JurcoachPage.objects.create(
-            body="Jurcoach Body",
-            title="Jurcoach Title",
-            depth=0,
-            poll=poll,
-            path="/"
-        )
-        self.case = Casetraining.objects.create(name="Advanced 1", difficulty="advanced", user=user, facts="Case facts")
-        Casetraining.objects.create(name="Beginner 1", difficulty="beginner", user=user)
-        Casetraining.objects.create(name="Shortcase 1", difficulty="shortcase", user=user)
+        Casetraining.objects.create(name="Beginner 1", difficulty="beginner", user=user, steps="", approved=True)
+        Casetraining.objects.create(name="Shortcase 1", difficulty="shortcase", user=user, steps="", approved=True)
+        self.case = Casetraining.objects.create(name="Advanced 1", difficulty="advanced", user=user, steps="", facts="Case facts", approved=True)
+        self.case_hidden = Casetraining.objects.create(name="Shortcase 2", difficulty="shortcase", user=user, steps="", approved=False)
 
     def test_index_response(self):
         response = self.client.get("/falltraining/")
@@ -45,8 +40,12 @@ class ViewTestCase(TestCase):
         self.assertContains(response, "Advanced 1")
         self.assertContains(response, "Beginner 1")
         self.assertContains(response, "Shortcase 1")
+        self.assertNotContains(response, "Shortcase 2")
 
     def test_show_response(self):
-        response = self.client.get("/falltraining/show/{}".format(self.case.id))
-        self.assertContains(response, "<h2>Advanced 1 (Niveau: advanced)</h2>", status_code=200)
-        self.assertContains(response, "Case facts")
+        response = self.client.get("/falltraining/show/{}/".format(self.case.id))
+        self.assertContains(response, "<case>", status_code=200)
+
+    def test_deny_api_access_to_not_approved_response(self):
+        response = self.client.get("/falltraining/api/case/{}.json".format(self.case_hidden.id))
+        self.assertContains(response, "", status_code=404)
