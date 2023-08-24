200965ab90d7b158d6d84fec0470140c3167c386
strafrecht-online <65466068+strafrecht-admin@users.noreply.github.com>
2023-08-24 10:18:32 +0200

    Update news.py

--- pages/models/news.py
+++ pages/models/news.py
@@ -123,7 +123,7 @@ class ArticlesPage(RoutablePageMixin, Page):
 
     template = 'page.html'
 
-    @route(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/(?P<day>[0-9]{2})/(?P<slug>[\w-]+)/?$')
+    @route(r'^(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/(?P<day>[0-9]{1,2})/(?P<slug>[\w-]+)/?$')
     def article_by_date(self, request, year, month, day, slug):
         article = get_object_or_404(
             ArticlePage,
