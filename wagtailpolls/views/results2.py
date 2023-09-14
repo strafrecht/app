f8f7121755aef20923ad294ff90308111c4f9372
strafrecht-online <65466068+strafrecht-admin@users.noreply.github.com>
2023-09-14 14:36:58 +0200

    Update results2.py

--- wagtailpolls/views/results2.py
+++ wagtailpolls/views/results2.py
@@ -2,7 +2,6 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 
 
-@permission_required('wagtailadmin.access_admin')
 def results2(request, poll_pk):
     from ..models import Poll, Vote
 
